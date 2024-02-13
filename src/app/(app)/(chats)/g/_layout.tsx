ac7df0712fd2e3fd85ddee74f6cbd3e8eef3b792
DominicGBauer <dominic@nomanini.com>
2024-02-13 13:25:55 +0200

    chore: update readme and remove files

--- src/app/(app)/(chats)/g/_layout.tsx
+++
@@ -1,5 +0,0 @@
-import { Slot } from "expo-router";
-
-export default function ChatsGLayout() {
-  return <Slot />;
-}
--- src/app/(app)/settings/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Stack } from "expo-router";
-
-export default function SettingsLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Settings" }} />
-    </Stack>
-  );
-}
